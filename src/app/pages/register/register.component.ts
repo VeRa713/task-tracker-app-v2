632f1fee5bc1e48fb25662a88d5ba105a76f6750
Vera David <vera.david1214@gmail.com>
2023-02-08 10:32:32 +0800

    changed register page to add user

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-
+  btnAddUser = () => {
+    console.log("Adding user to db...")
+  }
 }
