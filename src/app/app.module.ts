f1212802c9e48c330f4c95291a1746eba47ad3dc
Vera David <vera.david1214@gmail.com>
2023-02-08 11:22:33 +0800

    removed unused styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './sections/header/header.component';
 import { FooterComponent } from './sections/footer/footer.component';
 import { AddUserComponent } from './pages/add-user/add-user.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    AddUserComponent
+    AddUserComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule
