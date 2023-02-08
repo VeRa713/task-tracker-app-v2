00bf9afa4145df4ae8f67c4bb3bd7306fb5c37f3
Vera David <vera.david1214@gmail.com>
2023-02-08 10:40:07 +0800

    removed register and login pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './sections/header/header.component';
 import { FooterComponent } from './sections/footer/footer.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { LoginComponent } from './pages/login/login.component';
 import { ManagerLandingPageComponent } from './pages/manager-landing-page/manager-landing-page.component';
 import { UserLandingPageComponent } from './pages/user-landing-page/user-landing-page.component';
 
@@ -14,8 +12,6 @@ import { UserLandingPageComponent } from './pages/user-landing-page/user-landing
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    RegisterComponent,
-    LoginComponent,
     ManagerLandingPageComponent,
     UserLandingPageComponent
   ],
