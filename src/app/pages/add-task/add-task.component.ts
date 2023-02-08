7712cffaa63603131d87c57d0c701fc33035f98b
Vera David <vera.david1214@gmail.com>
2023-02-08 12:53:48 +0800

    added content to add task page

--- src/app/pages/add-task/add-task.component.ts
+++ src/app/pages/add-task/add-task.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-task.component.scss']
 })
 export class AddTaskComponent {
-
+  btnAddTask = () => {
+    console.log("Adding task....");
+  }
 }
