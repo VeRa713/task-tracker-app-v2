399487b9c45875bda781f12111c2ba0eb4f81d2e
Vera David <vera.david1214@gmail.com>
2023-02-08 10:46:12 +0800

    removed user & manager landing pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './sections/header/header.component';
 import { FooterComponent } from './sections/footer/footer.component';
-import { ManagerLandingPageComponent } from './pages/manager-landing-page/manager-landing-page.component';
-import { UserLandingPageComponent } from './pages/user-landing-page/user-landing-page.component';
 import { AddUserComponent } from './pages/add-user/add-user.component';
 
 @NgModule({
@@ -13,8 +11,6 @@ import { AddUserComponent } from './pages/add-user/add-user.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ManagerLandingPageComponent,
-    UserLandingPageComponent,
     AddUserComponent
   ],
   imports: [
