17e213fd6bf04a1b323491e73716060bd48d7316
Vera David <vera.david1214@gmail.com>
2023-02-21 09:22:57 +0800

    clear add user form

--- src/app/pages/add-user/add-user.component.ts
+++ src/app/pages/add-user/add-user.component.ts
@@ -1,16 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { UserService} from '../../services/user.service';
+import { User } from '../../models/user'
 
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.component.html',
   styleUrls: ['./add-user.component.scss']
 })
-export class AddUserComponent {
+export class AddUserComponent{
+
+  constructor(private userService : UserService) {}
+
+  @Input() user: User = {
+    id: 0,
+    firstName: "",
+    lastName: "",
+    email: ""
+  }
+
   btnAddUser = () => {
-    console.log("Adding user....");
+    console.log("Adding user....")
+    this.clearForm()
   }
 
   btnClear = () => {
-    console.log("Clearing add user form....");
+    console.log("Clearing add user form....")
+    this.clearForm()
+  }
+
+  clearForm(): void {
+    this.user.id = undefined
+    this.user.firstName = ""
+    this.user.lastName = ""
+    this.user.email = ""
   }
 }
