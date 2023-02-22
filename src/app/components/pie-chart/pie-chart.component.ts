6eda5ccdfbe772c91f0c9d2a0a65e1a713a7a155
Vera David <vera.david1214@gmail.com>
2023-02-22 12:33:19 +0800

    fixed indentation

--- src/app/components/pie-chart/pie-chart.component.ts
+++ src/app/components/pie-chart/pie-chart.component.ts
@@ -6,34 +6,32 @@ import { Chart } from 'chart.js/auto';
   templateUrl: './pie-chart.component.html',
   styleUrls: ['./pie-chart.component.scss'],
 })
-export class PieChartComponent implements OnInit{
+export class PieChartComponent implements OnInit {
   ngOnInit(): void {
     this.createChart();
   }
 
   public chart: any;
 
-  createChart = () =>{
-
-    this.chart = new Chart("MyChart", {
+  createChart = () => {
+    this.chart = new Chart('MyChart', {
       type: 'pie', //this denotes tha type of chart
 
-      data: {// values on X-Axis
-        labels: ['To do', 'In Progress','Done'],
-	       datasets: [{
-    label: 'Task Count',
-    data: [50, 25, 25],
-    backgroundColor: [
-      '#C5CAED',
-      '#C796DB',
-      '#9F63D0',
-    ],
-    hoverOffset: 4
-  }],
+      data: {
+        // values on X-Axis
+        labels: ['To do', 'In Progress', 'Done'],
+        datasets: [
+          {
+            label: 'Task Count',
+            data: [50, 25, 25],
+            backgroundColor: ['#C5CAED', '#C796DB', '#9F63D0'],
+            hoverOffset: 4,
+          },
+        ],
       },
       options: {
-        aspectRatio:1.5
-      }
+        aspectRatio: 1.5,
+      },
     });
-  }
+  };
 }
